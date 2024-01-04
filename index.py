f9c4892e6fdc2058467d9ce683dc6b1534927f3a
Superintendent <admin@superintendent.me>
2024-01-04 00:08:11 -0500

    Comments cause im dumb

--- index.py
+++ index.py
@@ -6,15 +6,17 @@ import inference
 import generate
 
 app = Flask(__name__)
-
+#Generate speech route
 @app.route('/generate_speech', methods=['POST'])
 def generate_speech():
+    #Messure time
     start_time = time.time()
     text = request.json.get('text')
     speeches = []
     for i in range(10):
         speech = generate.speech(text, cuda=False)
         end_time = timeit.default_timer()
+        #Messure time aswell
         execution_time = end_time - start_time
         print(f"The code executed in {execution_time} seconds")
         speeches.append(speech)
