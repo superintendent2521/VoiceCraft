73473373e7f464f502757920c70ddfc83ba7016c
Superintendent <admin@superintendent.me>
2024-01-04 13:36:38 -0500

    FEAT: Voice customization

--- index.py
+++ index.py
@@ -7,7 +7,8 @@ import inference
 import generate
 
 
-#en_speaker_3 is a nice brittish voice, set as default
+#en_speaker_3 is a nice brittish voice, set as default, can find the voice list here
+#https://suno-ai.notion.site/8b8e8749ed514b0cbf3f699013548683?v=bc67cff786b04b50b3ceb756fd05f68c
 voice = "v2/en_speaker_3"
 
 generate.speech("Kobold is pretty neat huh!", voice=voice)
