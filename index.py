0f03dba642d8fcebd5cdf61e66a7944862c62e1e
Superintendent <admin@superintendent.me>
2024-01-04 00:05:37 -0500

    that didnt do jackshit EREREGFGERERER, LOGGING TIME

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
+sys.path.insert(0, './src')
 from flask import Flask, jsonify, request
 import sys
-sys.path.insert(0, './src')
+import time
 import inference
 import generate
 
@@ -8,10 +9,14 @@ app = Flask(__name__)
 
 @app.route('/generate_speech', methods=['POST'])
 def generate_speech():
+    start_time = time.time()
     text = request.json.get('text')
     speeches = []
     for i in range(10):
         speech = generate.speech(text, cuda=False)
+        end_time = timeit.default_timer()
+        execution_time = end_time - start_time
+        print(f"The code executed in {execution_time} seconds")
         speeches.append(speech)
     return jsonify(speeches), 200
 
